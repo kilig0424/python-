dbef97c2bb1a8d14c11e59d86875d5104b83152b
kilig0424 <1191531153@qq.com>
2020-05-21 16:20:54 +0800

    重新在虚拟环境中创建了项目

--- Project/MyB2Cshop/MyB2Cshop/wsgi.py
+++ Project/MyB2Cshop/MyB2Cshop/wsgi.py
@@ -1,16 +1,16 @@
 """
-WSGI config for B2Cshop project.
+WSGI config for MyB2Cshop project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 """
 
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'B2Cshop.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "MyB2Cshop.settings")
 
 application = get_wsgi_application()
